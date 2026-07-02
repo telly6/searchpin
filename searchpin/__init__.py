ab9bc92afd284f8a8d2c46fde7f2027126ba0a71
telly6 <365297145@qq.com>
2026-07-02 21:38:37 +0800

    chore: bump to 1.0.2

--- searchpin/__init__.py
+++ searchpin/__init__.py
@@ -13,5 +13,5 @@ Usage:
 from searchpin.config import DEFAULT_MODEL_NAME, PRODUCT_NAME
 from searchpin.engine import MCP_TOOLS, SearchEngine
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __all__ = ["SearchEngine", "MCP_TOOLS", "PRODUCT_NAME", "DEFAULT_MODEL_NAME"]
